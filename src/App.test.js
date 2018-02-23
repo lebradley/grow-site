2752508b881e1acf8ba135af24cc9a210f0c9163
lbradley <lbradley@and.digital>
2018-02-23 11:33:38 +0000

    small test commit

--- src/App.test.js
+++ src/App.test.js
@@ -8,11 +8,11 @@ import App from './App';
 
 test('test test to pass test test', () => {
   const x = 1+1;
-  expect(x).to.equal(2);
-})
-
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(x).toBe(2);
 });
+
+// test('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
