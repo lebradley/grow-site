5323b24b4365a61a56885beea400fdde22e79a83
lbradley <lbradley@and.digital>
2018-02-23 11:30:51 +0000

    added gitignore and blocked node_modules, jest dependency set to previous to fix the test issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+/**
+ * @jest-environment node
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+test('test test to pass test test', () => {
+  const x = 1+1;
+  expect(x).to.equal(2);
+})
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
--- test/components/product.test.js
+++ test/components/product.test.js
@@ -2,6 +2,7 @@ const product = require('../../src/components/product');
 
 describe('some tests', () => {
     it('should test a thing', () => {
+        expect(1==1).to.equal(true);
         // put some test here
     });
 });
